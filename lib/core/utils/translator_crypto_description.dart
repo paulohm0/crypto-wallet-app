41fc4586cd7d92242e68b869575397ad4f1356d3
Paulo Henrique <paulohenrique1019@gmail.com>
2025-06-06 16:02:59 -0300

    fix: bug error translation description

--- lib/core/utils/translator_crypto_description.dart
+++ lib/core/utils/translator_crypto_description.dart
@@ -2,6 +2,14 @@ import 'package:translator/translator.dart';
 
 extension TranslatorExtension on String {
   Future<String> translateToPTBR() async {
+    final blockTranslation = RegExp(
+      r'not a supported language|not found|LanguageNotSupportedException',
+    );
+
+    if (blockTranslation.hasMatch(this)) {
+      return this;
+    }
+
     final translator = GoogleTranslator();
     final translation = await translator.translate(this, to: 'pt');
     return translation.text;
