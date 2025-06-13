71a83f1647e432939d6cf4783a8fb7f1ee54c331
Paulo Henrique <paulohenrique1019@gmail.com>
2025-06-13 20:03:47 -0300

    wip portfólio view

--- lib/ui/portfolio/view/portfolio_view.dart
+++ lib/ui/portfolio/view/portfolio_view.dart
@@ -1,4 +1,3 @@
-import 'package:crypto_wallet/shared/widgets/app_bar_custom.dart';
 import 'package:crypto_wallet/ui/portfolio/widgets/portfolio_action_buttons.dart';
 import 'package:crypto_wallet/ui/portfolio/widgets/portfolio_card.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +13,6 @@ class _PortfolioViewState extends State<PortfolioView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarCustom(),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(children: [PortfolioCard(), PortfolioActionButtons()]),
